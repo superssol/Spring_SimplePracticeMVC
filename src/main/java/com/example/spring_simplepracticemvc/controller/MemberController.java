1a674ea85ef940446aedba89423e680740f972f0
superssol <hansole10@gmail.com>
2022-11-17 15:07:36 +0900

    @Component 어노테이션을 이용한 빈 등록

---
+++ src/main/java/com/example/spring_simplepracticemvc/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.example.spring_simplepracticemvc.controller;
+
+import com.example.spring_simplepracticemvc.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
